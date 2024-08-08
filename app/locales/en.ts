21bff0d3a007d7d0477f88f8b7de95a8c34c13cb
ilya <ilyadorosh@gmail.com>
2024-08-08 14:29:40 +0200

    color

--- app/locales/en.ts
+++ app/locales/en.ts
@@ -127,7 +127,7 @@ const en: LocaleType = {
       "Resetting will clear the current conversation history and historical memory. Are you sure you want to reset?",
   },
   Home: {
-    NewChat: "New Chat",
+    NewChat: "New Q* Chat",
     DeleteChat: "Confirm to delete the selected conversation?",
     DeleteToast: "Chat Deleted",
     Revert: "Revert",
