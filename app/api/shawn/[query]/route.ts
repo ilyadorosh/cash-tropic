8e2846318a61cc78d4b1e6e628626a5d50828f9d
ilya <ilyadorosh@gmail.com>
2024-09-18 12:16:55 +0200

    impress shawn

--- app/api/shawn/[query]/route.ts
+++ app/api/shawn/[query]/route.ts
@@ -1,5 +1,13 @@
 import { NextResponse } from "next/server";
 
-export async function GET(req: Request) {
-  return NextResponse.json({ message: "Hello, World!" });
+export async function GET(req: Request, context: any) {
+  const { params } = context;
+
+  const p_output = "this will be a returned response from the api here";
+
+  return NextResponse.json({
+    message: "Hello, Shawn, please go to illige.fun/safespace/prompt!",
+    input: params,
+    output: p_output,
+  });
 }
