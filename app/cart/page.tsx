d8099a3009af0664cef3767a63e4b687af38ef5b
ilya <ilyadorosh@gmail.com>
2024-11-26 02:37:07 +0100

    fix checking conversations.message and types

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -12,10 +12,23 @@ import {
   showToast,
 } from "@/app/components/ui-lib";
 
+import { ChatMessage, useAppConfig, useChatStore } from "../store";
+
 // import { doSave } from "@/app/store/sync";
 
 export default async function Cart() {
   const pid = await kv.lrange("mylist", 0, 100000);
+
+  const conversations = pid
+    .map((item) => {
+      try {
+        return typeof item === "string" ? JSON.parse(item) : item;
+      } catch (error) {
+        console.error("Failed to parse item:", item, error);
+        return null;
+      }
+    })
+    .filter(Boolean);
 
   // Log the pid array to debug
   console.log("PID Array:", pid.length, " : len : ", pid);
@@ -51,15 +64,15 @@ export default async function Cart() {
     <div className={styles.chat}>
       <div className={styles["chat-body"]}>
         {/* {pid[0].messages[0].content} */}
-        {pid.map((conversation, index) => (
+        {conversations.map((conversation, index) => (
           <div className={styles["chat-message-container"]} key={index}>
             <div className={styles["chat-message-item"]}>
               {conversation.messages
-                .filter((message) => message.role === "user")
-                .map((message, i) => {
+                .filter((message: ChatMessage) => message.role === "user")
+                .map((message: ChatMessage, i: number) => {
                   return (
                     <div className={styles["chat-message-item"]} key={i}>
-                      {message.content}
+                      {message.content && <>{message.content}</>}
                     </div>
                   );
                 })}
