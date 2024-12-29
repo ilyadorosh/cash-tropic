024f8e54e77a4bf5c4a6740f3c872765fd384402
ilya <ilyadorosh@gmail.com>
2024-12-29 02:01:27 +0100

    add missing forgotten components

--- app/components-next/chat-list-next.tsx
+++ app/components-next/chat-list-next.tsx
@@ -4,6 +4,8 @@ import DeleteIcon from "../icons/delete.svg";
 import BotIcon from "../icons/bot.svg";
 
 import styles from "@/app/components/home.module.scss";
+
+import styles2 from "@/app/components/chat.module.scss";
 import {
   DragDropContext,
   Droppable,
@@ -22,6 +24,7 @@ import { MaskAvatar } from "@/app/components/mask";
 import { Mask } from "../store/mask";
 import { useRef, useEffect } from "react";
 import { showConfirm } from "@/app/components/ui-lib";
+import { SideBarBody, SideBarContainer } from "./sidebar-next";
 
 export function ChatItem(props: {
   onClick?: () => void;
@@ -173,5 +176,119 @@ export function ChatList(props: { narrow?: boolean }) {
         )}
       </Droppable>
     </DragDropContext>
+  );
+}
+
+export function ChatWTFElement(props: {
+  chats: { id: string; title: string }[];
+}) {
+  return (
+    <div className={styles2.chat}>
+      <div className={styles2["chat-body"]}>
+        <ChatWTF chats={props.chats} />
+      </div>
+    </div>
+  );
+}
+
+export function ChatWTF(props: { chats: { id: string; title: string }[] }) {
+  const onDragEnd: OnDragEndResponder = (result) => {
+    const { destination, source } = result;
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+  };
+  return (
+    <SideBarContainer
+      onDragStart={function (e: MouseEvent): void {
+        throw new Error("Function not implemented.");
+      }}
+      shouldNarrow={false}
+    >
+      <SideBarBody
+        onClick={(e) => {
+          if (e.target === e.currentTarget) {
+            // Use Next.js router
+          }
+        }}
+      >
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId="chat-list">
+            {(provided) => (
+              <div
+                className={styles["chat-list"]}
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+              >
+                {props.chats.map((item, index) => (
+                  <ChatWTFItem
+                    key={item.id}
+                    title={item.title}
+                    content={item.title}
+                    id={item.id}
+                    index={index}
+                  />
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </SideBarBody>
+    </SideBarContainer>
+  );
+}
+
+export function ChatWTFItem(props: {
+  onClick?: () => void;
+  onDelete?: () => void;
+  title: string;
+  content: string;
+  id: string;
+  index: number;
+}) {
+  const onDragEnd: OnDragEndResponder = (result) => {
+    const { destination, source } = result;
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+  };
+  return (
+    <Draggable draggableId={`${props.id}`} index={props.index}>
+      {(provided) => (
+        <div
+          className={`${styles["chat-item"]} ${styles["chat-item-selected"]}`}
+          onClick={props.onClick}
+          ref={(ele) => {
+            provided.innerRef(ele);
+          }}
+          title={`${props.title}\n`}
+        >
+          <>
+            <div className={styles["chat-item-title"]}>{props.title}</div>
+            <div className={styles["chat-item-info"]}>
+              <div className={styles["chat-item-count"]}>
+                {Locale.ChatItem.ChatItemCount(3)}
+                {/* {props.content} */}
+              </div>
+            </div>
+          </>
+        </div>
+      )}
+    </Draggable>
   );
 }
